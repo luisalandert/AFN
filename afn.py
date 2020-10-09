466e1f77a1adcbed2891c9e09178cd95de2aeb2c
luisalandert <luludila@gmail.com>
2020-10-08 22:26:56 -0300

    Uso do argparse

--- afn.py
+++ afn.py
@@ -1,3 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', dest='input',
+                    help="input file", metavar='INPUT_FILE')
+parser.add_argument('-o', dest='output',
+                    help='output file', metavar='OUTPUT_FILE')
+args = parser.parse_args()
+
 def set_state_transitions(transitions_list, state, symbols):
     resulting_states = []
     for symbol in symbols:
@@ -6,61 +15,70 @@ def set_state_transitions(transitions_list, state, symbols):
             if (i[0] == state) and (i[1] == symbol):
                 temp.append(str(i[2]))
         if not temp:
-            resulting_states.append(('-1',))
+            resulting_states.append('-1')
+        elif len(temp) == 1:
+            resulting_states.append(temp[0])
         else:
             resulting_states.append(tuple(temp))
     return resulting_states
 
-output = open('output.txt', 'a')
+# with open(args.output, 'a') as output:
+#     output.write("foi o teste\n")
 
-with open('input.txt') as txt:
-    num_of_automata = txt.readline()
+with open(args.input, 'r') as input:
+    num_of_automata = input.readline()
     for automata in range(int(num_of_automata)):
         print('automata:')
-        # LEITURA DADOS
-        description = txt.readline().rstrip().split(' ')
-        initial_state = description[3]
+
+        # LEITURA DADOS obs: todos os estados devem ser tuplas
+        description = input.readline().rstrip().split(' ')
+        initial_states = []
+        initial_states.append(description[3])
         states = range(int(description[0]))
         for i in states:
             states[i] = str(states[i])
         symbols = range(int(description[1]))
         for i in symbols:
             symbols[i] = str(symbols[i])
-        acception_states = txt.readline().rstrip().split(' ')
+        acception_states = input.readline().rstrip().split(' ')
         num_of_transitions = description[2]
         transitions = []
-        print('description: ')
-        print(description)
-        print('initial_state: ' + initial_state)
-        print('states: ')
-        print(states)
-        print('symbols: ')
-        print(symbols)
-        print('acception_state: ')
-        print(acception_states)
-        print('num_of_transitions: ' + num_of_transitions)
         for transition in range(int(num_of_transitions)):
-            transitions.append(txt.readline().rstrip().split(' '))
-        print('transitions: ')
-        print(transitions)
+            transitions.append(input.readline().rstrip().split(' '))
         # CONSTRUCAO AUTOMATO
         delta = {}
         for state in states:
             delta[state] = set_state_transitions(transitions, state, symbols)
-        print('delta:')
-        print(delta)
-        
         # TESTES
-        num_of_test_chains = txt.readline()
+        num_of_test_chains = input.readline()
         print('num_of_test_chains: ' + num_of_test_chains)
         test_chains = []
         for chain in range(int(num_of_test_chains)):
-            test_chains.append(txt.readline().rstrip().split(' '))
+            test_chains.append(input.readline().rstrip().split(' '))
+
+        print('description: ')
+        print(description)
+        print('initial_states: ')
+        print(initial_states)
+        print('states: ')
+        print(states)
+        print('symbols: ')
+        print(symbols)
+        print('acception_states: ')
+        print(acception_states)
+        print('num_of_transitions: ' + num_of_transitions)
+        print('transitions: ')
+        print(transitions)
+        print('delta:')
+        print(delta)
         print('test_chains: ')
         print(test_chains)
+input.close()
 
-        # output.write("\n")
-txt.close()
+# t = ('3',)
+# s = ''.join(t)
+
+
 
 
 
